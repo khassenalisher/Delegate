67ceb34a0187da8a2a7f6b58a44ab78e57d01b84
alish <khassenalisher@gmail.com>
2017-04-16 22:48:36 +0600

    Delegate

---
+++ TaskDelegate/Operation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDelegate
+{
+    class Operation
+    {
+        public delegate void DelegateMes(string message);
+        DelegateMes del;
+        public void RegDelegateMes(DelegateMes _del)
+        {
+            del = _del;
+        }
+        public Operation()
+        { }
+
+        public void Sum(int _a, int _b)
+        {
+            del((_a + _b).ToString());
+        }
+  }
+}
---
+++ TaskDelegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation op = new Operation();
+            op.RegDelegateMes(new Operation.DelegateMes(Show_Message));
+            op.Sum(10, 20);
+            Console.ReadLine();
+        }
+  
+        private static void Show_Message(String message)
+        {
+
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            Console.WriteLine(message);
+            
+        }
+
+    }
+}
